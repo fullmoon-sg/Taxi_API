d874b425c87daf4410a20144c3f84f67e08d1d09
fullmoon-sg <mbfoo7757@gmail.com>
2020-11-13 03:20:48 +0000

    Taxi in cluster

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-async function getTaxiCoordinates()
-{
-    let url = "https://api.data.gov.sg/v1/transport/taxi-availability";
-   let response = await axios.get(url);
-   return response.data;
+
+async function getTaxiCoordinates() {
+    let response = await axios.get('https://api.data.gov.sg/v1/transport/taxi-availability');
+    // x.features[0].geometry.coordinates
+    return response.data.features[0].geometry.coordinates; //Need to highlight what I need to return;
 }
 
-async function main()
+async function addTaxiMarkers(map)
 {
     let taxis = await getTaxiCoordinates();
-    let output = document.querySelector("#output");
-    console.log(taxis.features[0].geometry.coordinates.length);
-    for (let i =0; i < taxis.features[0].geometry.coordinates.length; i++ )
-    {
-        for (j=0; j < taxis.features[0].geometry.coordinates.length;j++)
-        {
-        let liElement = document.createElement('li');
-        liElement.innerHTML = taxis.features[0].geometry.coordinates[i][j];
-        output.appendChild(liElement);}
+    // create the cluster to hold the markers
+    let cluster = L.markerClusterGroup();
+    for (let t of taxis) {
+        // swap so that it becomes [lat, lng]
+        let coordinate = [t[1], t[0]];
+        let taxiMarker = L.marker(coordinate);
+        taxiMarker.addTo(cluster);
     }
-    
+    // add cluster to the map
+    cluster.addTo(map);
+
 }
 
-setInterval(function(){
-    main();
-}, 3000);
+// lat lng of Singapore
+let singapore = [1.29, 103.85];
+let map = L.map('map').setView(singapore, 10);
+
+// go to mapbox.com if we want to apply for our token
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+}).addTo(map);
+
+addTaxiMarkers(map);
