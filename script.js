d556cfcd4f875db549a36d276e126c59a657a864
fullmoon-sg <mbfoo7757@gmail.com>
2020-11-14 01:59:21 +0000

    New taxi script

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 
-async function getTaxiCoordinates() {
-    let response = await axios.get('https://api.data.gov.sg/v1/transport/taxi-availability');
-    // x.features[0].geometry.coordinates
-    return response.data.features[0].geometry.coordinates; //Need to highlight what I need to return;
+async function getTaxiCoord()
+{
+    let response = await axios.get("https://api.data.gov.sg/v1/transport/taxi-availability");
+    // x.features[0].geometry.coordinates. Ask for the return of the coordinate.
+    console.log(response.data.features[0].properties.taxi_count);
+    return response.data.features[0].geometry.coordinates; 
+    
 }
 
 async function addTaxiMarkers(map)
 {
-    let taxis = await getTaxiCoordinates();
-    // create the cluster to hold the markers
+    let taxis = await getTaxiCoord();
     let cluster = L.markerClusterGroup();
-    for (let t of taxis) {
-        // swap so that it becomes [lat, lng]
-        let coordinate = [t[1], t[0]];
-        let taxiMarker = L.marker(coordinate);
-        taxiMarker.addTo(cluster);
+    
+    for (let t of taxis)
+    {
+        let coordinate = [t[1],t[0]];
+        let taxisMarker = L.marker(coordinate);
+        taxisMarker.addTo(cluster)
     }
-    // add cluster to the map
+    //add cluster to map
     cluster.addTo(map);
-
 }
 
-// lat lng of Singapore
-let singapore = [1.29, 103.85];
-let map = L.map('map').setView(singapore, 10);
+//insert lat and long of Singapore
+let singapore = [1.29,103.85];
+let map = L.map("map").setView(singapore, 12);
 
 // go to mapbox.com if we want to apply for our token
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -35,4 +37,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
 
+
 addTaxiMarkers(map);
+
+ 
